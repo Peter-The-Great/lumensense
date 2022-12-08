f18c1f64e954ec8a4927d6af3b3392b5150e7f2e
bas0275 <bas.jans11@gmail.com>
2022-12-08 13:43:44 +0100

    s

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -2,18 +2,26 @@ package com.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class HelloApplication extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) {
         try {
 
             Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
             Scene scene = new Scene(root);
+
+            Image icon = new Image("file:img/logo.png");
+            stage.getIcons().add(icon);
+
             stage.setScene(scene);
             stage.show();
 
@@ -22,7 +30,5 @@ public class HelloApplication extends Application {
         }
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
