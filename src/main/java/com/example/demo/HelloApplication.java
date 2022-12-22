242e92ff65d4f2386bbf4c84c461863b057d436f
bas0275 <bas.jans11@gmail.com>
2022-12-22 12:11:28 +0100

    DB verbinding succesvol tot stand gebracht via server

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -7,12 +7,31 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
 
     public static void main(String[] args) {
+        String namedatabase = "lumensense";
+
+        String url ="jdbc:mysql://128.199.63.27/"+namedatabase+"";
+        String user ="LumensenseJavaApp";
+        String password ="Lumensense#Ln.T(RbbuKpF^.fMgixYd10";
+
+        try {
+            Connection connectdata = DriverManager.getConnection(url, user,password);
+            System.out.println("SUCCES");
+        } catch (SQLException ex) {
+            System.out.println("Error");
+            ex.printStackTrace();
+
+        }
+
         launch(args);
+
     }
     @Override
 
