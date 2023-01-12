d9ae6772501f71a20459f5c0639914717d1281ee
bas0275 <bas.jans11@gmail.com>
2023-01-12 10:52:40 +0100

    Error met fxml switch opgelost. Controller gefixt.

--- src/main/java/com/example/demo/StatsController.java
+++ src/main/java/com/example/demo/StatsController.java
@@ -2,26 +2,36 @@ package com.example.demo;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Time;
 import java.util.Date;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import javax.xml.transform.Result;
 
 
 public class StatsController implements Initializable {
+    public Stage stage;
+    public Scene scene;
+    public Parent root;
 
     @FXML public TableView<Stats> tableView;
     @FXML public TableColumn<Stats, Integer> id;
@@ -70,12 +80,19 @@ public class StatsController implements Initializable {
     }
 
 
-    public void switchToStats(ActionEvent event) {
-    }
+    public void switchToLogs(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("logs.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
-    public void switchToLogs(ActionEvent event) {
     }
-
-    public void switchToLight(ActionEvent event) {
+    public void switchToLight(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
