44faf0ecb863076e00b4449fd36a3441439c96e4
semkoolennl <sem.koolennl@gmail.com>
2023-01-16 11:36:48 +0100

    Added SQL functions and HTML parser test

--- src/main/java/com/example/demo/ConnectionDB.java
+++ src/main/java/com/example/demo/ConnectionDB.java
@@ -1,24 +1,22 @@
 package com.example.demo;
 
-import javax.swing.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConnectionDB {
 
-    private String namedatabase = "lumensense";
-
-    private String url = "jdbc:mysql://128.199.63.27/" + namedatabase + "";
-    private String user = "LumensenseJavaApp";
-    private String password = "Lumensense#Ln.T(RbbuKpF^.fMgixYd10";
+    private static Connection conn;
 
     public Connection getConnection() {
         try {
-//            Class.forName("org.h2.Driver");
-            Connection conn = DriverManager.getConnection(url, user, password);
+            String dbname   = "lumensense";
+            String url      = "jdbc:mysql://128.199.63.27/" + dbname + "";
+            String user     = "LumensenseJavaApp";
+            String password = "Lumensense#Ln.T(RbbuKpF^.fMgixYd10";
+
+            Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("SUCCES");
-            return conn;
+            conn = connection;
+            return connection;
         } catch (SQLException ex) {
             System.out.println("Error");
             ex.printStackTrace();
@@ -31,6 +29,40 @@ public class ConnectionDB {
         }
         System.out.println("Error connection db AFTER CATCH");
         return null;
+    }
+
+    public static boolean update(String query) {
+        try{
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(query);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static ResultSet select(String query) {
+        try{
+            Statement stmt = conn.createStatement();
+            return stmt.executeQuery(query);
+        } catch (SQLException e) {
+            System.out.println("Error in select query");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean updateLog(String content, String type, String lamp_id) throws SQLException {
+        // check if log exists
+        ResultSet rs = select("SELECT * FROM log WHERE type = '" + type + "' AND lamp_id = '" + lamp_id + "'");
+        if (rs != null && rs.next()) {
+            // update log
+            return update("UPDATE log SET content = '" + content + "' WHERE type = '" + type + "' AND lamp_id = '" + lamp_id + "'");
+        } else {
+            // insert log
+            return update("INSERT INTO log (content, type, lamp_id) VALUES ('" + content + "', '" + type + "', '" + lamp_id + "')");
+        }
     }
 }
 
---
+++ src/test/java/TestHTMLParser.java
@@ -0,0 +1,37 @@
+import org.jsoup.Jsoup;
+
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.util.ArrayList;
+
+public class TestHTMLParser {
+    public static void main(String[] args) {
+        File logFile = getLogFileFromMicrobit();
+        
+    }
+
+    private static File getLogFileFromMicrobit() {
+        File[] paths;
+        FileSystemView fsv = FileSystemView.getFileSystemView();
+
+        // returns pathnames for files and directory
+        paths = File.listRoots();
+
+        // for each pathname in pathname array
+        for(File path:paths)
+        {
+            String description = fsv.getSystemTypeDescription(path);
+            if (description.equals("USB Drive")) {
+                File[] files = path.listFiles();
+                assert files != null;
+                for (File file : files) {
+                    if (file.getName().equals("MY_DATA.HTM")) {
+                        return file;
+                    }
+                }
+            }
+
+        }
+        return null;
+    }
+}
